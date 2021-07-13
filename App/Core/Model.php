6a1a3fad1d89961fdbc427b947ba2dda67037311
unknown <rafanleme@gmail.com>
2021-07-13 11:25:15 -0300

    alterando conexao com o bd

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -8,8 +8,10 @@ class Model {
 
     public static function getConn(){
 
+        $password = $_ENV["database_password"];
+
         if(!isset(self::$conexao)){
-            self::$conexao = new \PDO("mysql:host=database-fastparking.cdhapnkytszh.us-east-1.rds.amazonaws.com;port=3306;dbname=fastparking;", "admin", "fast1234");
+            self::$conexao = new \PDO("mysql:host=database-fastparking.cdhapnkytszh.us-east-1.rds.amazonaws.com;port=3306;dbname=fastparking;", "admin", $password);
         }
 
         return self::$conexao;
