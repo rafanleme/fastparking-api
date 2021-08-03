8efa9ecf111a3d7bdbb1e96c37d78e5fb737ff63
Rafael Leme <rafanleme@gmail.com>
2021-08-03 10:59:08 -0300

    Mudando a senha

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -8,10 +8,8 @@ class Model {
 
     public static function getConn(){
 
-        $password = $_ENV["database_password"];
-
         if(!isset(self::$conexao)){
-            self::$conexao = new \PDO("mysql:host=database-fastparking.cdhapnkytszh.us-east-1.rds.amazonaws.com;port=3306;dbname=fastparking;", "admin", $password);
+            self::$conexao = new \PDO("mysql:host=database-fastparking.cdhapnkytszh.us-east-1.rds.amazonaws.com;port=3306;dbname=fastparking;", "admin", "bcd127");
         }
 
         return self::$conexao;
